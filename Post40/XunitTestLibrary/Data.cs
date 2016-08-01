51b80aff2d6bafb8bbef38be800166095766a58e
Illya Reznykov <ireznykov@hotmail.com>
2016-08-01 11:50:14 +0300

    Post40: add xunit test library

---
+++ Post40/XunitTestLibrary/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace XunitTestLibrary
+{
+	public class Data
+	{
+		private readonly object _lockObject = new object();
+
+		private int _state;
+
+		public int State
+		{
+			get { return _state; }
+			set
+			{
+				lock (_lockObject)
+				{
+					if (value < 0)
+						throw new ArgumentOutOfRangeException(nameof(State), "State should be positive");
+					_state = value;
+					// some inner changes
+				}
+			}
+		}
+	}
+}
---
+++ Post40/XunitTestLibrary/DataTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Xunit.Sdk;
+
+namespace XunitTestLibrary
+{
+	public class DataTests
+	{
+		public readonly string ExceptionMessage = $"State should be positive{Environment.NewLine}Parameter name: {nameof(Data.State)}";
+
+		[Theory]
+		[InlineData(0)]
+		[InlineData(1)]
+		public void Data_ShouldAccept_NonNegativeValue(int state)
+		{
+			Data data = null;
+			var exception = Record.Exception(() =>
+			{
+				data = new Data();
+				data.State = state;
+			});
+
+			data.Should().NotBeNull();
+			exception.Should().BeNull();
+		}
+
+		[Theory]
+		[InlineData(-1)]
+		public void Data_ShouldThrow_ExceptionOnNegativeValue(int state)
+		{
+			Data data = null;
+			var exception = Record.Exception(() =>
+			{
+				data = new Data();
+				data.State = state;
+			});
+
+			data.Should().NotBeNull();
+			exception.Should().NotBeNull();
+			exception.Message.Should().Be(ExceptionMessage);
+		}
+
+		[Theory]
+		[InlineData(-1)]
+		public void Data_ShouldThrow_ExceptionOnNegativeValueAndReturnNullObject(int state)
+		{
+			Data data = null;
+			Action task = () =>
+				{
+					data = new Data
+					{
+						State = state
+					};
+				};
+
+			var exception = Record.Exception(task);
+			data.Should().BeNull();
+			exception.Should().NotBeNull();
+			exception.Message.Should().Be(ExceptionMessage);
+		}
+
+		[Fact]
+		public async void Data_ShouldAccept_NonNegativeValueInAsync()
+		{
+			var data = new Data();
+			await Task.Run(() =>
+			{
+				for (var pos = 5; pos >= 0; pos--)
+				{
+					data.State = pos;
+				}
+			});
+
+			data.Should().NotBeNull();
+			data.State.Should().Be(0);
+		}
+
+		[Fact]
+		public void Data_ShouldNotThrow_ExceptionOnNonNegativeValueInAsync()
+		{
+			var data = new Data();
+			var task = Task.Run(() =>
+						{
+							for (var pos = 5; pos >= 0; pos--)
+							{
+								data.State = pos;
+							}
+						});
+
+			var taskException = Record.ExceptionAsync(async () => await task);
+			data.Should().NotBeNull();
+			data.State.Should().Be(0);
+			taskException.Should().NotBeNull();
+			taskException.Result.Should().BeNull();
+		}
+
+		[Fact]
+		public void Data_ShouldThrow_ExceptionOnNegativeValueInAsync()
+		{
+			var data = new Data();
+			var task = Task.Run(() =>
+						{
+							for (var pos = 1; pos >= -2; pos--)
+							{
+								data.State = pos;
+							}
+						});
+
+			var exception = Record.ExceptionAsync(async () => await task);
+
+			data.Should().NotBeNull();
+			data.State.Should().Be(0);
+			exception.Should().NotBeNull();
+			exception.Result.Should().NotBeNull();
+			exception.Result.Message.Should().Be(ExceptionMessage);
+		}
+
+		[Fact]
+		public void Data_ShouldThrow_ExceptionOnNegativeStateInTwoAsyncTasks()
+		{
+			var data = new Data();
+			var tasks = new Task[]
+				{
+			Task.Run(() =>
+				{
+					for (var pos = 0; pos < 10; pos++)
+					{
+						data.State += 1;
+					}
+				}),
+			Task.Run(() =>
+				{
+					for (var pos = 0; pos < 20; pos++)
+					{
+						data.State -= 1;
+					}
+				}),
+				};
+
+			var exception = Record.ExceptionAsync(async () => await Task.WhenAll(tasks));
+			data.Should().NotBeNull();
+			exception.Should().NotBeNull();
+			exception.Result.Should().NotBeNull();
+			exception.Result.Message.Should().Be(ExceptionMessage);
+		}
+	}
+}
