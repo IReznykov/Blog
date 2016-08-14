c2f498ec4f7644d40153ff5b2d182579f474224d
Illya Reznykov <ireznykov@hotmail.com>
2016-08-14 19:21:22 +0300

    Post42: add code for post "Square WPF user control"

---
+++ Square Wpf user control/WpfApplication/Converters/MinDoubleConverter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApplication.Converters
+{
+	/// <summary>
+	/// Returns minimal value of input double values.
+	/// </summary>
+	[ValueConversion(typeof(double[]), typeof(double))]
+	public class MinDoubleConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType,
+			object parameter, CultureInfo culture)
+		{
+			Debug.Assert(targetType.IsAssignableFrom(typeof(double)),
+				$"targetType should be {typeof(double).FullName}");
+
+			// values:
+			if (values == null || values.Length <= 0)
+				return DependencyProperty.UnsetValue;
+
+			var tValues = Convert(values, culture);
+
+			// check that all input values were not wrong
+			if (tValues.Count <= 0)
+				return DependencyProperty.UnsetValue;
+
+			// return aggregate value
+			var tResult = tValues.Min();
+
+			// check parameter and round resulting value
+			var roundDigits = 0;
+			if (Convert(parameter, culture,
+				(value1, culture1) => Math.Max(0, System.Convert.ToInt32(value1)),
+				ref roundDigits))
+			{
+				tResult = Math.Round(tResult, roundDigits);
+			}
+
+			return System.Convert.ChangeType(tResult, targetType);
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes,
+			object parameter, CultureInfo culture)
+		{
+			// no back conversion
+			return null;
+		}
+
+		private static IList<double> Convert(object[] values, CultureInfo culture)
+		{
+			var tValues = new List<double>(values.Length);
+			foreach (var value in values)
+			{
+				double result = 0;
+
+				if (Convert(value, culture,
+					(value1, culture1) =>
+					{
+						var result1 = System.Convert.ToDouble(value1);
+						return double.IsNaN(result1) ? 0 : result1;
+					},
+					ref result))
+				{
+					tValues.Add(result);
+				}
+			}
+
+			return tValues;
+		}
+
+		private static bool Convert<T>(object value, CultureInfo culture,
+			Func<object, CultureInfo, T> convertFunc, ref T tResult)
+		{
+			try
+			{
+				if (value == null || value.Equals(DependencyProperty.UnsetValue))
+					return false;
+				tResult = convertFunc(value, culture);
+			}
+			catch (FormatException ex)
+			{
+				// ignore it, some wrong input value
+				Debug.Assert(false, DebugMessage<T>(value, ex));
+			}
+			catch (InvalidCastException ex)
+			{
+				// ignore it, some wrong input value
+				Debug.Assert(false, DebugMessage<T>(value, ex));
+			}
+			catch (OverflowException ex)
+			{
+				// ignore it, some wrong input value
+				Debug.Assert(false, DebugMessage<T>(value, ex));
+			}
+			return true;
+		}
+
+		private static string DebugMessage<T>(object value, Exception ex)
+		{
+			return
+				$"value \"{value?.ToString() ?? "null"}\", type {value?.GetType().FullName ?? "null"}" +
+				$" should be convertible to type {typeof(T).FullName}. Exception: {ex.Message}";
+		}
+	}
+}
---
+++ Square Wpf user control/WpfApplication/Views/CellControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApplication.Views
+{
+	/// <summary>
+	/// Interaction logic for CellControl
+	/// </summary>
+	public partial class CellControl : UserControl
+	{
+		public CellControl()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Start brush for gradient filling - the most light color of the cell.
+		/// Default was #FFE0E1F0
+		/// </summary>
+		public Color StartColor
+		{
+			get { return (Color)GetValue(StartColorProperty); }
+			set { SetValue(StartColorProperty, value); }
+		}
+
+		public static readonly DependencyProperty StartColorProperty =
+			DependencyProperty.Register(
+				"StartColor",
+				typeof(Color),
+				typeof(CellControl),
+				new PropertyMetadata(Color.FromArgb(255, Colors.WhiteSmoke.R, Colors.WhiteSmoke.G, Colors.WhiteSmoke.B)));
+
+		/// <summary>
+		/// Start Color for gradient filling - the most light color of the cell.
+		/// Default was #FF000766
+		/// </summary>
+		public Color FinishColor
+		{
+			get { return (Color)GetValue(FinishColorProperty); }
+			set { SetValue(FinishColorProperty, value); }
+		}
+
+		public static readonly DependencyProperty FinishColorProperty =
+			DependencyProperty.Register(
+				"FinishColor",
+				typeof(Color),
+				typeof(CellControl),
+				new PropertyMetadata(Color.FromArgb(255, Colors.DarkBlue.R, Colors.DarkBlue.G, Colors.DarkBlue.B)));
+
+	}
+}
